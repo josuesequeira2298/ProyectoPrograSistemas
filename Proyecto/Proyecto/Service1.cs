f32467d3d6490323c518dcafda6cc1d8568f1cf5
josuesequeira2298 <josue_2298_Outlook.com>
2018-12-01 21:26:36 -0600

    Commit que ud no pudo hacer xDD

--- Proyecto/Proyecto/Service1.cs
+++ Proyecto/Proyecto/Service1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Proyecto
 {
@@ -19,6 +20,24 @@ namespace Proyecto
 
         protected override void OnStart(string[] args)
         {
+            var timer = new Timer { AutoReset = true, Interval = 60000 };
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        private void timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var processes = Process.GetProcessesByName("firefox");
+
+            foreach (var process in processes)
+            {
+                process.CloseMainWindow();
+                if (!process.HasExited)
+                {
+                    process.Kill();
+                    process.Close();
+                }
+            }
         }
 
         protected override void OnStop()
